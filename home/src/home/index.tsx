8d9538ccf90e76a804bc0f5881dd3789c3afd25e
eheu <eheudev@gmail.com>
2021-04-19 19:16:34 +0200

    Rename

--- home/src/home/index.tsx
+++ home/src/home/index.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import "./app.css";
+import "./home.css";
+
+const mount = "home"
 
 const App = () => <div>Hi there, I'm React from Webpack 5.</div>;
 
-ReactDOM.render(<App />, document.getElementById("app"));
-  
+ReactDOM.render(<App />, document.getElementById(mount));
