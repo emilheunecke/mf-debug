fba154b257ff2f3a7ad01b6a8690b3f80a2d5e10
eheu <eheudev@gmail.com>
2021-04-19 19:09:59 +0200

    Add home template

---
+++ home/src/app/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./app.css";
+
+const App = () => <div>Hi there, I'm React from Webpack 5.</div>;
+
+ReactDOM.render(<App />, document.getElementById("app"));
+  
